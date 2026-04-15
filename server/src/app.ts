5e00a29325219169567b6d2ae3789ba9f7e430d2
aditya umesh <umeshaditya72@gmail.com>
2026-04-15 20:52:43 +0530

    new

--- server/src/app.ts
+++ server/src/app.ts
@@ -14,9 +14,9 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.ORIGIN,
     credentials: true,
-  })
+  }),
 );
 
 app.use(express.json());
@@ -29,6 +29,4 @@ app.use("/api/codeBook", codeBookRouter);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () =>
-  console.log(`🚀 Server running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
